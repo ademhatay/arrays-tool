62a0bccb9339672f0f964cdb20b88fa8282c7394
Adem Hatay <hatayadem5@gmail.com>
2023-05-29 00:10:03 +0300

    tested shuffle

---
+++ src/tests/shuffleArray.test.ts
@@ -0,0 +1,14 @@
+import { shuffle } from '../functions/shuffleArray';
+
+
+describe('shuffle', () => {
+  it('should shuffle the array', () => {
+    const array = [45, 23, 6, 12, 13, 2, 67, 1, 0, 3, 4, 5, 6, 7, 8, 9]
+
+	const result = shuffle(array);
+
+    expect(result).not.toEqual(array);
+    expect(result.length).toBe(array.length);
+    expect(new Set(result)).toEqual(new Set(array));
+  });
+});
